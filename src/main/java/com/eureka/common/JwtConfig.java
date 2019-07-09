b31e990700d6e4f469cd07460041ec0b00358060
O_CETIN3 <O_CETIN3@TD38460.thynet.thy.com>
2019-07-09 15:59:53 +0300

    first

---
+++ src/main/java/com/eureka/common/JwtConfig.java
@@ -0,0 +1,28 @@
+package com.eureka.common;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+
+/**
+ * Created by INNGKISAKOL on 1.07.2019.
+ */
+@Data
+public class JwtConfig {
+
+    // Spring doesn't inject/autowire to "static" fields.
+    // Link: https://stackoverflow.com/a/6897406
+    @Value("${security.jwt.uri:/auth/**}")
+    private String Uri;
+
+    @Value("${security.jwt.header:Authorization}")
+    private String header;
+
+    @Value("${security.jwt.prefix:Bearer }")
+    private String prefix;
+
+    @Value("${security.jwt.expiration:#{24*60*60}}")
+    private int expiration;
+
+    @Value("${security.jwt.secret:JwtSecretKey}")
+    private String secret;
+}
---
+++ src/main/java/com/eureka/common/model/UserRole.java
@@ -0,0 +1,9 @@
+package com.eureka.common.model;
+
+/**
+ * Created by INNGKISAKOL on 1.07.2019.
+ */
+public enum UserRole
+{
+    USER, ADMIN;
+}
